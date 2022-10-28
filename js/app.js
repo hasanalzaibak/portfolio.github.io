2f35e386450f3d0bb5ec18409133a61c84a3784f
hasanalzaibak <hasan.zaibak@hotmail.com>
2022-10-28 10:14:56 -0300

    JS button function

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const contactButton = document.querySelector('#contact-button')
+
+contactButton.addEventListener('click', () => {
+    window.location.href = 'mailto:hasan.n.alzaibak@gmail.com'
+})
